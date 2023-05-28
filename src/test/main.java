8857356c0b75f0dc861e24d2b897637306cd1d99
Yoonyoung Park <pyy2114@naver.com>
2023-05-29 02:22:45 +0900

    First Commit

---
+++ src/test/main.java
@@ -0,0 +1,13 @@
+package test;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Git Test");
+		System.out.println("Git Test");
+		System.out.println("Git Test");
+		System.out.println("Git Test");
+	}
+
+}
